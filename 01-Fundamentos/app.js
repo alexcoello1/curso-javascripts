892ff45177184991fca2d96071f843abe1ef51fa
Alexcoello <webmayansolutions.github.com>
2020-05-31 17:10:05 -0500

    curso del javascripts
    curso de javascripts fundamentos

---
+++ 01-Fundamentos/app.js
@@ -0,0 +1,18 @@
+//string
+var primerNombre = "Grover";
+console.log(primerNombre);
+//int
+var edad = 34;
+var sueldo = 1800.56;
+
+console.log(edad);
+//logico
+var tieneTrabajo = true;
+console.log(tieneTrabajo);
+//variable indefinida
+var puestodeTrabajo;
+console.log(puestodeTrabajo);
+//variable null
+tieneTrabajo = null;
+
+console.log(tieneTrabajo);
